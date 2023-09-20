1e41cd75d051f14a7f6808c17bdec17536b90751
Joel DSouza <joeldsouzax@gmail.com>
2023-09-20 16:49:54 +0200

    refactor(ui): added cover picture for phone screens

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components";
 import { getEvents, getRootCategories, getUserRole } from "./action";
 import { AdminTypes, adminUsers, crudUsers, normalUsers } from "@/util";
+import Image from "next/image";
 
 const Events: NextPage = async () => {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -21,21 +22,31 @@ const Events: NextPage = async () => {
   if (normalUsers.includes(role)) {
     const categories = await getRootCategories(supabase);
     return (
-      <section className="container px-2 mt-10 max-w-6xl">
-        <CardList
-          data={categories}
-          errorMessage="No Categories Found!"
-        >
-          {({ id, is_member, title, description }) => (
-            <Card
-              key={id}
-              url={"/category/" + id}
-              title={title!}
-              description={description!}
-              isMember={is_member!}
-            />
-          )}
-        </CardList>
+      <section className="container  md:mt-10 max-w-6xl flex flex-col gap-6 items-center">
+        <div className="w-full flex flex-col items-center">
+          <Image
+            src={"/cover.jpeg"}
+            alt="bodrum-festival"
+            width={600}
+            height={200}
+          />
+        </div>
+        <section className="px-2">
+          <CardList
+            data={categories}
+            errorMessage="No Categories Found!"
+          >
+            {({ id, is_member, title, description }) => (
+              <Card
+                key={id}
+                url={"/category/" + id}
+                title={title!}
+                description={description!}
+                isMember={is_member!}
+              />
+            )}
+          </CardList>
+        </section>
       </section>
     );
   }
--- components/user/UserLogin.tsx
+++ components/user/UserLogin.tsx
@@ -1,17 +1,30 @@
 "use client";
 import * as React from "react";
 import LoginScan from "./LoginScan";
+import { BsQrCodeScan } from "react-icons/bs";
+import { FaWpforms } from "react-icons/fa";
 
 const UserLogin: React.FC = () => {
   const [showAdminLogin, setShowAdminLogin] = React.useState(false);
   return (
     <div className="card-body flex flex-col items-center w-full">
-      <input
-        type="checkbox"
-        className="toggle toggle-accent toggle-lg"
-        checked={showAdminLogin}
-        onChange={(e) => setShowAdminLogin((s) => !s)}
-      />
+      <label className="btn btn-circle swap swap-rotate">
+        {/* this hidden checkbox controls the state */}
+        <input
+          type="checkbox"
+          onChange={(e) => setShowAdminLogin((s) => !s)}
+          checked={showAdminLogin}
+        />
+
+        <FaWpforms
+          className="swap-on fill-current"
+          size={30}
+        />
+        <BsQrCodeScan
+          className="swap-off fill-current"
+          size={30}
+        />
+      </label>
       {showAdminLogin ? (
         <form
           action="/auth/sign-in"
