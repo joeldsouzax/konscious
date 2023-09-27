93c4fc01b23d5ed9f58f13b30c07dad4dac7f63d
Joel DSouza <joeldsouzax@gmail.com>
2023-09-27 21:03:40 +0200

    refactor(login): added reload

--- components/user/LoginScan.tsx
+++ components/user/LoginScan.tsx
@@ -9,12 +9,14 @@ interface LoginScanProps {}
 const LoginScan: React.FC<LoginScanProps> = () => {
   const [loading, setLoading] = React.useState(false);
   const ref = React.useRef<HTMLDivElement>(null);
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 1, qrbox: { width: 400, height: 400 } },
-    /* verbose= */ false
-  );
+
   React.useEffect(() => {
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+      "reader",
+      { fps: 1, qrbox: { width: 400, height: 400 } },
+      /* verbose= */ false
+    );
+
     html5QrcodeScanner.render(
       (text, result) => {
         setLoading(true);
@@ -43,30 +45,11 @@ const LoginScan: React.FC<LoginScanProps> = () => {
           form.submit();
         }
         // // create the form
-        // const form = document.createElement("form");
-        // form.setAttribute("hidden", "true");
-        // form.setAttribute("method", "post");
-        // form.setAttribute("encType", "multipart/form-data");
-        // form.setAttribute("action", "/auth/sign-in");
-        // // create email
-        // const email = document.createElement("input");
-        // email.setAttribute("type", "email");
-        // email.setAttribute("name", "email");
-        // email.setAttribute("value", userData.email);
-        // form.appendChild(email);
-        // // create password
-        // const password = document.createElement("input");
-        // password.setAttribute("type", "password");
-        // password.setAttribute("name", "password");
-        // password.setAttribute("value", userData.hash);
-        // form.appendChild(password);
-
-        // document.body.appendChild(form);
-
-        // form.submit();
 
         setTimeout(() => {
+          window.location.reload();
           setLoading(false);
+
           html5QrcodeScanner.resume();
         }, 1000);
       },
