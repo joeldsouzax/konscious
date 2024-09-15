a53f8a47900db228a7f46bad5af1eefd62825a44
Joel DSouza <joeldsouzax@gmail.com>
2024-09-15 11:16:17 +0200

    fixed canvas problem with nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
     utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in with pkgs; {
-        devShells.default = mkShell { buildInputs = [ nodejs pnpm ]; };
+        devShells.default = mkShell {
+          buildInputs = [ nodejs pnpm darwin.apple_sdk.frameworks.CoreText ];
+          shellHook = ''
+            export NIX_LDFLAGS="-F${darwin.apple_sdk.frameworks.CoreFoundation}/Library/Frameworks -framework CoreFoundation $NIX_LDFLAGS";
+          '';
+        };
       });
 }
